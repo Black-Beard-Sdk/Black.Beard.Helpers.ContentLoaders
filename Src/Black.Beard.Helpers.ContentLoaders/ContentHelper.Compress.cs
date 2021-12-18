21e66633f0b12175e4947cb8abf50543e7ef0e91
gael beard <gaelgael5@gmail.com>
2021-12-18 19:26:27 +0100

    Initialise solution

---
+++ Src/Black.Beard.Helpers.ContentLoaders/ContentHelper.Compress.cs
@@ -0,0 +1,44 @@
+﻿using SharpCompress.Common;
+using SharpCompress.Readers;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Bb
+{
+
+    public static partial class ContentHelper
+    {
+
+        /// <summary>
+        /// Uncompress a compressed file in a target folder
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputDir"></param>
+        public static void Uncompress(this FileInfo inputFile, DirectoryInfo outputDir)
+        {
+
+            if (!outputDir.Exists)
+                outputDir.Create();
+
+            var option = new ExtractionOptions()
+            {
+                ExtractFullPath = true,
+                Overwrite = true
+            };
+
+            var outpath = outputDir.FullName;
+
+            using (Stream stream = inputFile.OpenRead())
+            using (var reader = ReaderFactory.Open(stream))
+                while (reader.MoveToNextEntry())
+                    if (!reader.Entry.IsDirectory)
+                        //Console.WriteLine(reader.Entry.Key);
+                        reader.WriteEntryToDirectory(outpath, option);
+
+        }
+
+    }
+
+
+}
